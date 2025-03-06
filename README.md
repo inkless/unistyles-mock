7fc7005ecb21694416f403157f27ce532792182f
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2025-03-05 17:13:48 -0800

    update

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Download the code to your own project or if you want to:
 
 ```bash
-pnpm add inkless/unistyles-mock
+pnpm add inkless/unistyles-mock -D
 ```
 
 Create a `__mocks__/react-native-unistyles.js` in your project root.
--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('jest').Config} */
 const config = {
-  testEnvironment: "node",
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   // An array of regexp pattern strings that are matched against all test paths
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  transformIgnorePatterns: [
-    "node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-unistyles|react-native-nitro-modules|react-native-svg|@affirm))",
-  ],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transformIgnorePatterns: ['node_modules/(?!(?:.pnpm/)?((jest-)?react-native'],
 };
 
 export default config;
